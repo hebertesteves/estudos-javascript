e059a7dc7e8a168146754e80451b5e9b032ebe7e
Hebert Esteves <hebertesteves14.sp@gmail.com>
2024-09-30 12:51:31 -0300

    Adicionando Aula 4, 5 e 6 do Modulo 3 de DOM

--- B7Web/dom/script.js
+++ B7Web/dom/script.js
@@ -1,5 +1,33 @@
-let t1 = document.getElementsByTagName('h1')[0];
-let txt = document.getElementById('teste');
-let btn = document.getElementsByClassName('botao')[0];
-let lista1 = document.querySelectorAll('#teste ul li');
-let btn1 = document.querySelector('btn');
+/* Selecionando Elementos:
+
+document.getElementsByTagName('h1')[0];
+document.getElementById('teste');
+document.getElementsByClassName('botao')[0];
+document.querySelectorAll('#teste ul li');
+document.querySelector('btn');
+
+*/
+
+/* Adicionar Evento via JavaScript
+let botao = document.querySelector('.botao');
+botao.addEventListener('click', () => {
+    clicou();
+});
+*/
+
+function clicou() {
+    const teste = document.querySelector('#teste');
+    const ul = teste.querySelector('ul');
+
+    ul.children[0].innerHTML = 'Item 1 <strong>alterado!</strong>';
+
+    ul.children[1].append("(alterado)");
+
+    ul.innerHTML += "<li> Item alterado</li>";
+
+    let newLi = document.createElement("li");
+    newLi.innerText = 'Item adicionado!!!';
+    ul.appendChild(newLi);
+    ul.prepend(newLi);
+}
+
