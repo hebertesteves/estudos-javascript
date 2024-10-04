0ef2598e4ea57e30884dc8f745a9ec7ab9d54148
Hebert Esteves <hebertesteves14.sp@gmail.com>
2024-10-04 17:03:47 -0300

    Aula 7 e Aula 8 do Modulo 3 da B7Web

--- B7Web/dom/script.js
+++ B7Web/dom/script.js
@@ -29,5 +29,41 @@ function clicou() {
     newLi.innerText = 'Item adicionado!!!';
     ul.appendChild(newLi);
     ul.prepend(newLi);
+
+    const newButton = document.createElement('button');
+    newButton.innerHTML = 'Botão';
+
+    let newUl = document.createElement('ul');
+    for (let i = 0; i < 5; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerHTML = `Item add ${i + 1}`;
+        newUl.append(newLi);
+    }
+
+    ul.after(newButton); // After = Depois
+    ul.before(newUl); // Before = Antes
+
+    const input = document.querySelector('input');
+
+    console.log(input.getAttribute('type'));
+    if (input.hasAttribute('placeholder')) {
+        console.log("Tem placeholder sim");
+    } else {
+        console.log("Não tem placeholder");
+    }
+    input.setAttribute('placeholder', 'Placeholder alterado');
+
+    const botao = document.querySelector('.botao');
+    if (input.getAttribute('type') == 'text') {
+        input.setAttribute('type', 'password');
+        botao.innerHTML = 'Mostrar senha';
+    } else {
+        input.setAttribute('type', 'text');
+        botao.innerHTML = 'Esconder senha';
+
+    }
+    
+    
+    
 }
 
